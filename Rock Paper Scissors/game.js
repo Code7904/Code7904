47249131df18f7b420e093a9ba4a7cae6dc08f68
Code7904 <akartik7904@gmail.com>
2025-01-27 14:09:04 +0530

    readme in rock paper scissores

--- Rock Paper Scissors/game.js
+++ Rock Paper Scissors/game.js
@@ -1,25 +1,29 @@
-let userScore = 0;
-let compScore = 0;
+let userScore = 0; // initial user scores number
+let compScore = 0; // initial comp scores number
 
-const choices = document.querySelectorAll(".choice");
-const msg = document.querySelector("#msg");
+const choices = document.querySelectorAll(".choice");  // choice
+const msg = document.querySelector("#msg"); // game msg
 
-const userScorePara = document.querySelector("#user-score");
-const compScorePara = document.querySelector("#comp-score");
+const userScorePara = document.querySelector("#user-score"); // user scores number
+const compScorePara = document.querySelector("#comp-score"); // comp scores number
 
 
-
+// computer choices
 const genCompChoice = () => {
     const options = ["rock", "paper", "scissors"];
     const randIdx = Math.floor(Math.random() * 3);
     return options[randIdx];
-};
+}; 
 
+
+// dram match 
 const drawGame = () => {
     msg.innerText = "Game Draw! Play Again!!";
     msg.style.backgroundColor = "tomato";
 };
 
+
+// sh0w msg if win or lose or draw
 const showWinner = (userWin, userChoice, compChoice) => {
     if (userWin) {
         userScore++;
@@ -34,25 +38,27 @@ const showWinner = (userWin, userChoice, compChoice) => {
     }
 };
 
+
+// proper match function which conduct game 
 const playGame = (userChoice) => {
-
-    const compChoice = genCompChoice();
-
+    const compChoice = genCompChoice(); // draw
     if (userChoice === compChoice) {
         drawGame();
     } else {
         let userWin = true;
         if (userChoice === "rock") {
-            userWin = compChoice === "paper" ? false : true
-        } else if (userChoice === "paper") {
-            userWin = compChoice === "scissors" ? false : true
+            userWin = compChoice === "paper" ? false : true // 1st win case
+        } else if (userChoice === "paper") { 
+            userWin = compChoice === "scissors" ? false : true // 2nd win case
         } else {
-            userWin = compChoice === "rock" ? false : true
+            userWin = compChoice === "rock" ? false : true // last win case
         }
         showWinner(userWin, userChoice, compChoice);
     }
 };
 
+
+// user choices
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
         const userChoice = choice.getAttribute("Id");
